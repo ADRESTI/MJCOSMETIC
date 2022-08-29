ec4480f971500b2cd9841e20e62cf0f3f4a7df7b
ADRESTI <soyeon8489@daum.net>
2022-08-29 23:20:19 +0900

    협업프로젝트 서브담당

---
+++ js/fade.js
@@ -0,0 +1,29 @@
+$(function(){
+    const $mainRight = $(".main_content01_right > div > img");
+
+    // $mainRight.on('click', function(){
+    //     alert('클릭되었습니다.');
+    // })
+
+    const $scrollTop = $('')
+
+    let nowIdx = null;
+
+    
+
+
+
+    $(window).scroll(function(){
+        $mainRight.each(function(i){
+            
+            let rightElement = $(this).offset().top = $(this).outerHeight();
+            let bottomWin = $(window).scrollTop() + $(window).height();
+
+            if( bottomWin > rightElement){
+                $(this).animate({'opacity':'1','margin-left':'0px'},1000);
+            }
+        })
+
+    })
+
+})
